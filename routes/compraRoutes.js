9f2253dfe0cbb54a8ef6e954b63692ba1153ee31
bhyanx <bhyanxdev@gmail.com>
2025-04-29 00:47:57 -0500

    mejora ruta compras

--- routes/compraRoutes.js
+++ routes/compraRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const detalleCompraController = require('../controllers/compraController');
 
 // Rutas para DetalleCompra
-router.get('/', detalleCompraController.getAllCompras); // Obtener todos los detalles de compras
+router.get('/', detalleCompraController.getAll); // Obtener todos los detalles de compras
 router.get('/:id', detalleCompraController.getById); // Obtener un detalle de compra por ID
 router.post('/', detalleCompraController.create); // Crear un nuevo detalle de compra
 router.put('/:id', detalleCompraController.update); // Actualizar un detalle de compra
