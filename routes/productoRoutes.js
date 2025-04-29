e48c22c0344cc28b032e6a718635d16974ea943f
bhyanx <bhyanxdev@gmail.com>
2025-04-29 01:00:22 -0500

    correccion

--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
 
-router.get('/', productoController.getAllProductos); // Obtener todos los productos
-router.get('/:id', productoController.getProductoById); // Obtener un producto por ID
-router.post('/', productoController.createProducto); // Crear un nuevo producto
-router.put('/:id', productoController.updateProducto); // Actualizar un producto
-router.delete('/:id', productoController.deleteProducto); // Eliminar un producto (eliminación lógica)
+// Rutas para Producto
+router.get('/', productoController.getAll); // Obtener todos los productos
+router.get('/:id', productoController.getById); // Obtener un producto por ID
+router.post('/', productoController.create); // Crear un nuevo producto
+router.put('/:id', productoController.update); // Actualizar un producto
+router.delete('/:id', productoController.delete); // Eliminar un producto
+
 module.exports = router;
